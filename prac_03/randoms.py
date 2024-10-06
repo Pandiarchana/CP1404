ee7af4ed16e8c971426e6d8c8b40b54ae79eafd3
ASUS1 <pandiarchana.palaniappan@my.jcu.edu.au>
2024-10-06 19:56:37 +0800

    randoms.py is completed

---
+++ prac_03/randoms.py
@@ -0,0 +1,21 @@
+import random
+
+print(random.randint(5, 20))
+print(random.randint(3, 10, 2))
+print(random.uniform(2.5, 5.5))
+
+"""
+line 1: 13,
+#smallest number: 5
+#highest number: 20
+
+line 2: 7,
+#smallest number: 3
+#highest number: 9
+
+line 3: 3.0824
+#smallest number: 2.5
+#highest number: 5.5
+
+"""
+
