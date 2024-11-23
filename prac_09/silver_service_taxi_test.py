b38c9fa17aea2fb56fa58ae255e241b4aa81b0ae
ASUS1 <pandiarchana.palaniappan@my.jcu.edu.au>
2024-11-23 21:52:49 +0800

    The silver service taxi test file completed for the car file program.

---
+++ prac_09/silver_service_taxi_test.py
@@ -0,0 +1,15 @@
+from prac_09.taxi import Taxi
+
+
+class SilverServiceTaxi(Taxi):
+    flag_fall = 4.50
+
+    def __init__(self, name, fuel, fanciness):
+        super().__init__(name, fuel)
+        self.price_per_km *= fanciness
+
+    def __str__(self):
+        return f"{super().__str__()} plus flag_fall of ${self.flagfall:.2f}"
+
+    def get_fare(self):
+        return super().get_fare() + self.flagfall
